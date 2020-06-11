f4b07338fa171a00bd8586042fa9cd9ca7e0fd90
Unnati Kolhe <unnatikolhe@gmail.com>
2020-06-11 19:01:05 +0530

    Added Public

---
+++ public/date.js
@@ -0,0 +1,13 @@
+exports.getDate = function() {
+
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
